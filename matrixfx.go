9e294d4060f074efeacb39f68ce5ec1ecb1b51cf
eibd <70450641+eibd@users.noreply.github.com>
2024-10-24 23:26:40 -0300

    add japanese characters

--- matrixfx.go
+++ matrixfx.go
@@ -14,9 +14,13 @@ const (
 var (
 	width       = os.Getpagesize()
 	coefficient = width / 20
+	hiragana    = []rune("ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをん")
+	katakana    = []rune("ァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵヶ")
+	kanji       = []rune("一二三四五六七八九十百千万山川田人心力日月火水木金土王天気花雨")
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	hideCursor()
 	defer showCursor()
 
@@ -24,7 +28,7 @@ func main() {
 		matrix := generateMatrix()
 		printMatrix(matrix)
 		shiftMatrixDown(matrix)
-		time.Sleep(150 * time.Millisecond)
+		time.Sleep(200 * time.Millisecond)
 	}
 }
 
@@ -44,12 +48,24 @@ func generateMatrix() [][]rune {
 			if rand.Intn(10) > 2 {
 				row[j] = ' '
 			} else {
-				row[j] = rune(rand.Intn(2) + 48)
+				row[j] = randomJapaneseCharacter()
 			}
 		}
 		matrix[i] = row
 	}
 	return matrix
+}
+
+func randomJapaneseCharacter() rune {
+	set := rand.Intn(3)
+	switch set {
+	case 0:
+		return hiragana[rand.Intn(len(hiragana))]
+	case 1:
+		return katakana[rand.Intn(len(katakana))]
+	default:
+		return kanji[rand.Intn(len(kanji))]
+	}
 }
 
 func printMatrix(matrix [][]rune) {
@@ -68,6 +84,6 @@ func shiftMatrixDown(matrix [][]rune) {
 		for row := height - 1; row > 0; row-- {
 			matrix[row][col] = matrix[row-1][col]
 		}
-		matrix[0][col] = rune(rand.Intn(94) + 33)
+		matrix[0][col] = randomJapaneseCharacter()
 	}
 }
